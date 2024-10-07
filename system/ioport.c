0503a5a96e4837c7bb113cbc0a15e3c2ad70f384
wjq <w@w>
2024-10-07 15:52:27 +0200

    io handled

--- system/ioport.c
+++ system/ioport.c
@@ -50,6 +50,7 @@ static uint64_t unassigned_io_read(void *opaque, hwaddr addr, unsigned size)
 {
     //// --- Begin LibAFL code ---
     io_handled = false;
+    printf("pci :%x not handled???\n",addr);
     //// --- End LibAFL code ---
 
     return -1ULL;
