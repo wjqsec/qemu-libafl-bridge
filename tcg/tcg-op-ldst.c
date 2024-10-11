4971e9603e785aefb610df708841e97662e12802
wjq <w@w>
2024-10-11 23:22:50 +0200

    mem rw value

--- tcg/tcg-op-ldst.c
+++ tcg/tcg-op-ldst.c
@@ -215,7 +215,7 @@ static void tcg_gen_qemu_ld_i32_int(TCGv_i32 val, TCGTemp *addr,
     TCGv_i64 temp_out =  tcg_temp_ebb_new_i64();
     TCGv_i64 low64_val = tcg_temp_ebb_new_i64();
     tcg_gen_extu_i32_i64(low64_val, val);
-    gen_helper_libafl_qemu_pre_memrw(temp_out,temp_tcgv_i64(addr),tcg_constant_i64(memop_size(get_memop(orig_oi))), tcg_constant_i32(0), low64_val, tcg_constant_i64(0));
+    gen_helper_libafl_qemu_pre_memrw(temp_out,temp_tcgv_i64(addr),tcg_constant_i64(memop_size(get_memop(orig_oi))), tcg_constant_i32(0), tcg_constant_i64(0), tcg_constant_i64(0));
     addr = tcgv_i64_temp(temp_out);
     //// --- End LibAFL code ---
     if ((memop & MO_BSWAP) && !tcg_target_has_memory_bswap(memop)) {
@@ -288,7 +288,7 @@ static void tcg_gen_qemu_st_i32_int(TCGv_i32 val, TCGTemp *addr,
     TCGv_i64 temp_out =  tcg_temp_ebb_new_i64();
     TCGv_i64 low64_val = tcg_temp_ebb_new_i64();
     tcg_gen_extu_i32_i64(low64_val, val);
-    gen_helper_libafl_qemu_pre_memrw(temp_out,temp_tcgv_i64(addr),tcg_constant_i64(memop_size(get_memop(orig_oi))), tcg_constant_i32(1), low64_val, tcg_constant_i64(0));
+    gen_helper_libafl_qemu_pre_memrw(temp_out,temp_tcgv_i64(addr),tcg_constant_i64(memop_size(get_memop(orig_oi))), tcg_constant_i32(1), tcg_constant_i64(0), tcg_constant_i64(0));
     addr = tcgv_i64_temp(temp_out);
     //// --- End LibAFL code ---
     if ((memop & MO_BSWAP) && !tcg_target_has_memory_bswap(memop)) {
