bf0cb468966cf1074d266044be344e8f9988f915
wjq <w@w>
2024-10-15 22:13:44 +0200

    snapshot free bug test

--- libafl/syx-snapshot/syx-snapshot.c
+++ libafl/syx-snapshot/syx-snapshot.c
@@ -175,9 +175,9 @@ void syx_snapshot_free(SyxSnapshot* snapshot)
         increment = syx_snapshot_increment_free(increment);
     }
 
-    return;
+    
     g_hash_table_remove_all(snapshot->rbs_dirty_list);
-
+    return;
     syx_snapshot_root_free(snapshot->root_snapshot);
 
     g_free(snapshot);
