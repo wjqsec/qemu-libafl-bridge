5b1eeeef3f5624036893af96269c477ab2ceb74e
wjq <w@w>
2024-12-11 16:10:50 +0100

    smm enter test

--- hw/isa/lpc_ich9.c
+++ hw/isa/lpc_ich9.c
@@ -518,6 +518,7 @@ static void ich9_lpc_rcba_update(ICH9LPCState *lpc, uint32_t rcba_old)
         memory_region_del_subregion(get_system_memory(), &lpc->rcrb_mem);
     }
     if (rcba & ICH9_LPC_RCBA_EN) {
+        printf("aaaaaaaaaaaa %p\n",lpc->rcrb_mem->owner);
         memory_region_add_subregion_overlap(get_system_memory(),
                                             rcba & ICH9_LPC_RCBA_BA_MASK,
                                             &lpc->rcrb_mem, 1);
