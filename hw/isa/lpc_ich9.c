6bc93e53f5f048eb7855bc0d2a213cd14e5defb3
wjq <w@w>
2024-12-11 16:53:47 +0100

    smm enter test

--- hw/isa/lpc_ich9.c
+++ hw/isa/lpc_ich9.c
@@ -459,7 +459,7 @@ static void ich9_lpc_pm_init(ICH9LPCState *lpc)
 static void ich9_apm_ctrl_changed(uint32_t val, void *arg)
 {
     ICH9LPCState *lpc = arg;
-
+    printf("yyyyyyyyyyyyyyyyyyyy\n");
     /* ACPI specs 3.0, 4.7.2.5 */
     acpi_pm1_cnt_update(&lpc->pm.acpi_regs,
                         val == ICH9_APM_ACPI_ENABLE,
