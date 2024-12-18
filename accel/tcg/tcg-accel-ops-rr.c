c5e8526774dbc4680f04b78799df4975bcb5c6c3
wjq <w@w>
2024-12-18 20:50:49 +0100

    remove wait for io event

--- accel/tcg/tcg-accel-ops-rr.c
+++ accel/tcg/tcg-accel-ops-rr.c
@@ -298,7 +298,7 @@ static void *rr_cpu_thread_fn(void *arg)
             qemu_notify_event();
         }
 
-        rr_wait_io_event();
+        // rr_wait_io_event();
         rr_deal_with_unplugged_cpus();
     }
 
