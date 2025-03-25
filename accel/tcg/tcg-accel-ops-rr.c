abb279514f5867d304b4d90ed82fb793e4d54d28
wjq <w@w>
2025-03-25 13:19:46 +0100

    dead lock fix

--- accel/tcg/tcg-accel-ops-rr.c
+++ accel/tcg/tcg-accel-ops-rr.c
@@ -297,7 +297,7 @@ static void *rr_cpu_thread_fn(void *arg)
              */
             qemu_notify_event();
         }
-        printf("rr_cpu_thread_fn\n");
+
         rr_wait_io_event();
         rr_deal_with_unplugged_cpus();
     }
